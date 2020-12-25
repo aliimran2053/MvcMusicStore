5c066467186ee1b4291019af983eceb90242ae92
aliimran2053 <76563973+aliimran2053@users.noreply.github.com>
2020-12-25 19:39:08 +0530

    Update HomeController.cs

--- MvcMusicStore/Controllers/HomeController.cs
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -27,7 +27,7 @@ namespace MvcMusicStore.Controllers
             // the albums with the highest count
 
             return storeDB.Albums
-                .OrderByDescending(a => a.OrderDetails.Count())
+                .OrderBy(a => a.OrderDetails.Count())
                 .Take(count)
                 .ToList();
         }
